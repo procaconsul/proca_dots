4fb06eda7060e19b9c0c8eea3d1b397e87b5e6d1
Ivan Procaccini <ivanprocaccini905@gmail.com>
2022-03-21 17:59:47 +0000

    Use https to install plugins from git

--- plugins/oh-my-zsh_plugins.sh
+++ plugins/oh-my-zsh_plugins.sh
@@ -4,7 +4,7 @@
 
 OMZ_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git \
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
   $OMZ_CUSTOM/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
   $OMZ_CUSTOM/themes/powerlevel10k
